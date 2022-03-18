c01deee913c3a664d6af644abce53caf799203ca
jiffylube <lu.jeffrey91@gmail.com>
2022-03-18 17:43:44 -0400

    bird and blocks created, background created

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
 let bird = document.querySelector('.bird')
+
+let singleJump = 45;
+
+let jump = function () {
+  singleJump += 50;
+  bird.style.bottom = singleJump + 'px';
+}
+  
+  let spaceBar = function (e) {
+    if (e.keyCode === 32) {
+      jump()
+    }
+  }
+  
+document.addEventListener('keypress', spaceBar)
